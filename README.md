096e9abc2f214c19a0ad7160edc72398c4d117f8
AnhDungPham290198 <dungpham290198@gmail.com>
2025-11-06 15:43:45 +0700

    commit 1.1 result

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
-# How to run the project
+# Virtual environment setup
 1. Install uv (example: on macOS)
 ```
 brew install uv
 ```
-2. Install the dependencies
+2. Create and activate the virtual environment
+```
+uv venv --python 3.12 
+source .venv/bin/activate
+```
+3. Install the dependencies
 ```
 uv sync
+```
+
+# .env file
+```
+GITHUB_TOKEN_1=your_github_token_1
+GITHUB_TOKEN_2=your_github_token_2
+```
+
+
+# Command to build human PR commit details dataframe
+First time run:
+```
+python processing/build_human_pr_commit_details_df.py -o data/output.parquet
+```
+Resume from previous run:
+```
+python processing/build_human_pr_commit_details_df.py -o data/output.parquet --resume
 ```
---
+++ config.py
@@ -0,0 +1,38 @@
+# config path for the project using relative path from the root of the project
+import os
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+GITHUB_DATA_DIR = os.path.join(DATA_DIR, "github")
+
+# Basic
+PR_DF_PATH = "hf://datasets/hao-li/AIDev/pull_request.parquet"
+REPO_DF_PATH = "hf://datasets/hao-li/AIDev/repository.parquet"
+USER_DF_PATH = "hf://datasets/hao-li/AIDev/user.parquet"
+
+# Comments and reviews
+PR_COMMENTS_DF_PATH = "hf://datasets/hao-li/AIDev/pr_comments.parquet"
+PR_REVIEWS_DF_PATH = "hf://datasets/hao-li/AIDev/pr_reviews.parquet"
+PR_REVIEW_COMMENTS_DF_PATH = "hf://datasets/hao-li/AIDev/pr_review_comments_v2.parquet"
+
+# Commits
+PR_COMMITS_DF_PATH = "hf://datasets/hao-li/AIDev/pr_commits.parquet"
+PR_COMMIT_DETAILS_DF_PATH = "hf://datasets/hao-li/AIDev/pr_commit_details.parquet"
+
+# Related issues
+RELATED_ISSUE_DF_PATH = "hf://datasets/hao-li/AIDev/related_issue.parquet"
+ISSUE_DF_PATH = "hf://datasets/hao-li/AIDev/issue.parquet"
+
+# Events
+PR_TIMELINE_DF_PATH = "hf://datasets/hao-li/AIDev/pr_timeline.parquet"
+
+# Task type
+PR_TASK_TYPE_DF_PATH = "hf://datasets/hao-li/AIDev/pr_task_type.parquet"
+
+# Human-PR
+HUMAN_PR_DF_PATH = "hf://datasets/hao-li/AIDev/human_pull_request.parquet"
+HUMAN_PR_TASK_TYPE_DF_PATH = "hf://datasets/hao-li/AIDev/human_pr_task_type.parquet"
+HUMAN_PR_COMMIT_DETAILS_DF_PATH = os.path.join(GITHUB_DATA_DIR, "output", "human_pr_commit_details_df.parquet")
+
+
+# Analysis files
+ANALYSIS_1_1_DF_PATH = os.path.join(DATA_DIR, "analysis", "analysis_1_1_df.parquet")
